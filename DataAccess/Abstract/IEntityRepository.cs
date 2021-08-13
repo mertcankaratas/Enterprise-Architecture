c02de85709ebe16a384472e7fd552e258a7ee9b3
Mertcan Karatas <can.mert.karatas@gmail.com>
2021-08-13 17:05:01 +0300

    Efproduct Dolduruldu

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-   public interface IEntityRepository<T>
+    //Generic Constraint
+    //class : referans tip
+    //IEntity : IEntity olabilir  veya IEntity olanbir nesne olabilir
+    //new(): new'lene bilir olmalı 
+   public interface IEntityRepository<T> where T: class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);

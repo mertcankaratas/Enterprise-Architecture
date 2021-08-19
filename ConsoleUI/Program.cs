2a4f6e108ed74c2a5d23ff99d428e2d3126f304a
Mertcan Karatas <can.mert.karatas@gmail.com>
2021-08-19 19:15:07 +0300

    Core Yazıldı

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,13 +9,30 @@ namespace ConsoleUI
     {
         //SOLID
         //Open Closed Principle
+        //IoC
+        //Data Transformation Object
         static void Main(string[] args)
+        {
+            ProductTest();
+            //CatergoryTest();
+        }
+
+        private static void CatergoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
             }
         }
     }
--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
 
     //IEntity implemenet eden class bir veritabanı tablosudur

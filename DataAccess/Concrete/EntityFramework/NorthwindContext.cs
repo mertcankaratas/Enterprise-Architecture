6b8175ca6b8c9da1c047826f8eb85914a5367565
Mertcan Karataş <can.mert.karatas@gmail.com>
2021-08-23 16:28:23 +0300

    sürüm değişikliği yapıldı

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -11,7 +11,8 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+            optionsBuilder.UseSqlServer(@"Server=desktop-9d6tod0\sqlexpress;Database=Northwind;Trusted_Connection=true");
+            
         }
 
         public DbSet<Product> Products { get; set; }

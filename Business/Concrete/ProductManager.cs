24753c2c492be8e1983d3055a720952be45825b0
MERTCAN KARATAŞ <can.mert.karatas@gmail.com>
2021-09-05 19:31:40 +0300

    Business Rules yazıldı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,36 +2,43 @@
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
-using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
-using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
-using FluentValidation;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
         IProductDal _ProductDal;
+        ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal, ICategoryService categoryService)
         {
             _ProductDal = productDal;
+            _categoryService = categoryService;
         }
 
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {   //business codes
-           
-          
-          
+
+            IResult result = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.CategoryId), CheckIfProductNameExist(product.ProductName), CheckIfCategoryLimitExceded());
+            if (result != null)
+            {
+                return result;
+            }
             _ProductDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
+
+
+
+
         }
 
 
@@ -68,11 +75,47 @@ namespace Business.Concrete
             return new SuccessDataResult<List<ProductDetailDto>>(_ProductDal.GetProductDetails());
         }
 
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
         IDataResult<List<Product>> IProductService.GetByUnitPrice(decimal min, decimal max)
         {
             throw new NotImplementedException();
         }
 
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            //select count(*) from products where catergoryId=1
+            var result = _ProductDal.GetAll(p => p.CategoryId == categoryId).Count;
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            return new SuccessResult();
+        }
 
+        private IResult CheckIfProductNameExist(string productName)
+        {
+            var result = _ProductDal.GetAll(p => p.ProductName == productName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count > 15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+            return new SuccessResult();
+        }
     }
 }
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -5,12 +5,15 @@ using System.Text;
 
 namespace Business.Constants
 {
-  public static  class Messages
+    public static class Messages
     {
         public static string ProductAdded = "Ürün eklendi";
         public static string ProductNameInvalid = "Ürün İsmi geçersiz";
 
-        public static string MaintenanceTime ="Sistem bakımda";
-       public static string ProductsListed = "Ürünler listelendi";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Kategorideki ürün limitini aştınız";
+        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded ="Categori limit aşıldı";
     }
 }
